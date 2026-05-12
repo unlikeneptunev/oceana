1393855d5519591d18bfb76538f7b231c8b04575
Kekensya <kensya1205@gmail.com>
2026-05-12 18:48:56 +0700

    feat: add dashboard UI layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
 }
